9defa5b34c807d9c92aab79d863c6291c61b64d3
DylanW-Dev <dylan.wanten.dev@gmail.com>
2025-02-12 00:28:23 +0100

    Implementation of the different sections as well as a footer via import of Component

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <Footer>
+            <p>&copy;Event Planner Organization. All Rights Reserved</p>
+        </Footer>
+    );
+}
+
+export default Footer;
